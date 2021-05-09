1442fca45bc205512ace51a92bf57801f1fc910a
60050211 <43135341+Praeee@users.noreply.github.com>
2021-05-09 14:27:19 +0700

    ui Rigister

---
+++ lib/utils/variables.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+mystyle(double size,[Color color,FontWeight fw]){
+  return GoogleFonts.montserrat(
+    fontSize: size,
+    fontWeight: fw,
+    color: color
+  );
+}
