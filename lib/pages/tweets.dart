9d0be08eb5680055dc9f2b9515474041c11af82b
60050211 <43135341+Praeee@users.noreply.github.com>
2021-05-24 10:40:36 +0700

    add bottom navigation

---
+++ lib/pages/tweets.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:social/utils/variables.dart';
+
+class TweetsPage extends StatefulWidget {
+  @override
+  _TweetsPageState createState() => _TweetsPageState();
+}
+
+class _TweetsPageState extends State<TweetsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Home",style: mystyle(20),),
+    
+    );
+  }
+}
