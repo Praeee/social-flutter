ef8596c7f358276b77364ec24ce0c86b9252cbde
60050211 <43135341+Praeee@users.noreply.github.com>
2021-05-28 10:59:10 +0700

    user profile

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:social/comment.dart';
+import 'package:social/utils/variables.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -6,10 +11,335 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  String uid;
+  Stream userstream;
+  String username = '';
+  String profilepic;
+  bool dataisthere = false;
+  initState() {
+    super.initState();
+    getcurrentuseruid();
+    getstream();
+    getcurrentuserinfo();
+  }
+
+  getcurrentuserinfo() async {
+    var firebaseuser = FirebaseAuth.instance.currentUser;
+    DocumentSnapshot userdoc = await usercollection.doc(firebaseuser.uid).get();
+    // var followersdocuments = await usercollection
+    //     .doc(firebaseuser.uid)
+    //     .collection('followers')
+    //     .get();
+    // var followngdocuments = await usercollection
+    //     .doc(firebaseuser.uid)
+    //     .collection('following')
+    //     .get();
+    // usercollection
+    //     .doc(firebaseuser.uid)
+    //     .collection('followers')
+    //     .doc(firebaseuser.uid)
+    //     .get()
+    //     .then((doc) {
+    //   if (doc.exists) {
+    //     setState(() {
+    //       isfollowing = true;
+    //     });
+    //   } else {
+    //     setState(() {
+    //       isfollowing = false;
+    //     });
+    //   }
+    // });
+    setState(() {
+      username = userdoc.data()['username'];
+      // following = followngdocuments.docs.length;
+      // followers = followersdocuments.docs.length;
+      profilepic = userdoc.data()['profilepic'];
+      dataisthere = true;
+    });
+  }
+
+  
+
+  getstream() async {
+    var firebaseuser = FirebaseAuth.instance.currentUser;
+    setState(() {
+      userstream =
+          tweetcollection.where('uid', isEqualTo: firebaseuser.uid).snapshots();
+    });
+  }
+
+  getcurrentuseruid() async {
+    var firebaseuser = FirebaseAuth.instance.currentUser;
+    setState(() {
+      uid = firebaseuser.uid;
+    });
+  }
+
+  //likepost
+  likepost(String documentid) async {
+    var firebaseuser = FirebaseAuth.instance.currentUser;
+    DocumentSnapshot doc = await tweetcollection.doc(documentid).get();
+
+    if (doc.data()['likes'].contains(firebaseuser.uid)) {
+      tweetcollection.doc(documentid).update({
+        'likes': FieldValue.arrayRemove([firebaseuser.uid])
+      });
+    } else {
+      tweetcollection.doc(documentid).update({
+        'likes': FieldValue.arrayUnion([firebaseuser.uid])
+      });
+    }
+  }
+
+//sharepost
+  sharepost(String documentid, String tweet) async {
+    Share.text('Flitter', tweet, 'text/plain');
+    DocumentSnapshot doc = await tweetcollection.doc(documentid).get();
+    tweetcollection
+        .doc(documentid)
+        .update({'shares': doc.data()['shares'] + 1});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: dataisthere == true ? SingleChildScrollView(
+        physics: ScrollPhysics(),
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 4,
+              decoration: BoxDecoration(
+                gradient:
+                    LinearGradient(colors: [Colors.lightBlue, Colors.white]),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 6,
+                  left: MediaQuery.of(context).size.width / 2 - 64),
+              child: CircleAvatar(
+                radius: 64,
+                backgroundColor: Colors.white,
+                backgroundImage: NetworkImage(profilepic),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 2.9),
+              child: Column(
+                children: [
+                  Text(
+                    username,
+                    style: mystyle(30, Colors.black, FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "Following",
+                        style: mystyle(20, Colors.black, FontWeight.w600),
+                      ),
+                      Text(
+                        "Followes",
+                        style: mystyle(20, Colors.black, FontWeight.w600),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "12",
+                        style: mystyle(20, Colors.black, FontWeight.w600),
+                      ),
+                      Text(
+                        "20",
+                        style: mystyle(20, Colors.black, FontWeight.w600),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                              colors: [Colors.lightBlue, Colors.blue])),
+                      child: Center(
+                        child: Text(
+                          "Edit Profile",
+                          style: mystyle(20, Colors.white, FontWeight.w700),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "User tweets",
+                    style: mystyle(20, Colors.black, FontWeight.w700),
+                  ),
+                  StreamBuilder(
+                    stream: userstream,
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return CircularProgressIndicator();
+                        }
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: snapshot.data.docs.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              DocumentSnapshot tweetdoc =
+                                  snapshot.data.docs[index];
+                              return Card(
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    backgroundImage:
+                                        NetworkImage(tweetdoc['profilepic']),
+                                  ),
+                                  title: Text(
+                                    tweetdoc['username'],
+                                    style: mystyle(
+                                        20, Colors.black, FontWeight.w600),
+                                  ),
+                                  subtitle: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      if (tweetdoc['type'] == 1)
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            tweetdoc['tweet'],
+                                            style: mystyle(20, Colors.black,
+                                                FontWeight.w400),
+                                          ),
+                                        ),
+                                      if (tweetdoc['type'] == 2)
+                                        Image(
+                                            image: NetworkImage(
+                                                tweetdoc['image'])),
+                                      if (tweetdoc['type'] == 3)
+                                        Column(
+                                          children: [
+                                            Text(
+                                              tweetdoc.data()['tweet'],
+                                              style: mystyle(20, Colors.black,
+                                                  FontWeight.w400),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Image(
+                                                image: NetworkImage(
+                                                    tweetdoc.data()['image'])),
+                                          ],
+                                        ),
+                                      SizedBox(height: 10),
+                                      Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                InkWell(
+                                                  onTap: () => Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              CommentPage(
+                                                                  tweetdoc.data()[
+                                                                      'id']))),
+                                                  child: Icon(Icons.comment),
+                                                ),
+                                                SizedBox(
+                                                  width: 10.0,
+                                                ),
+                                                Text(
+                                                  tweetdoc
+                                                      .data()['commentcount']
+                                                      .toString(),
+                                                  style: mystyle(18),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                InkWell(
+                                                  onTap: () => likepost(
+                                                      tweetdoc.data()['id']),
+                                                  child: tweetdoc
+                                                          .data()['likes']
+                                                          .contains(uid)
+                                                      ? Icon(
+                                                          Icons.favorite,
+                                                          color: Colors.red,
+                                                        )
+                                                      : Icon(Icons
+                                                          .favorite_border),
+                                                ),
+                                                SizedBox(
+                                                  width: 10.0,
+                                                ),
+                                                Text(
+                                                  tweetdoc
+                                                      .data()['likes']
+                                                      .length
+                                                      .toString(),
+                                                  style: mystyle(18),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              children: [
+                                                InkWell(
+                                                  onTap: () => sharepost(
+                                                      tweetdoc.data()['id'],
+                                                      tweetdoc.data()['tweet']),
+                                                  child: Icon(Icons.share),
+                                                ),
+                                                SizedBox(
+                                                  width: 10.0,
+                                                ),
+                                                Text(
+                                                  tweetdoc
+                                                      .data()['shares']
+                                                      .toString(),
+                                                  style: mystyle(18),
+                                                ),
+                                              ],
+                                            ),
+                                          ])
+                                    ],
+                                  ),
+                                ),
+                              );
+                            });
+                      })
+                ],
+              ),
+            )
+          ],
+        ),
+      ): Center(child: CircularProgressIndicator())
     );
   }
 }
